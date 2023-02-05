1f250c825c709d674b2c21b3c68d9a42636523fc
kk-macbook <deceiver0123@yahoo.co.jp>
2023-02-05 21:13:02 +0400

    from Max

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, current_app, g, request, redirect, flash
+from email_validator import validate_email, EmailNotValidError
+import logging
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
-
+app.config["SECRET_KEY"] = "2AZSMss3p5QPcY2hBsJ"
+#ログレベルを設定する
+app.logger.setLevel(logging.DEBUG)
+#リダイレクトを中断しないようにする
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+#DebugToolbarExtensionにアプリケーションをセットする
+toolbar = DebugToolbarExtension(app)
 
 @app.route("/")
 def index():
@@ -17,6 +26,49 @@ def hello(name):
 def show_name(name):
     return render_template("index.html", name=name)
 
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/contact/complete", methods=["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]
+
+        #入力チェック
+        is_valid = True
+
+        if not username:
+            flash("ユーザ名は必須です")
+            is_valid = False
+
+        if not email:
+            flash("メールアドレスは必須です")
+            is_valid = False
+        
+        try:
+            validate_email(email)
+        except EmailNotValidError:
+            flash("メールアドレスの形式で入力してください")
+            is_valid = False
+        
+        if not description:
+            flash("問い合わせ内容は必須です")
+            is_valid = False
+        
+        if not is_valid:
+            return redirect(url_for("contact"))
+        
+
+
+
+        return redirect(url_for("contact_complete"))
+
+    return render_template("contact_complete.html")
 
 with app.test_request_context():
     # /
